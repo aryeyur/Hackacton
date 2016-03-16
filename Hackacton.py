01773cff61b47b969270cbb1b862d58c86e847b3
max <maxyor6040@gmail.com>
2016-03-16 21:44:33 +0200

    log in page 4 arye
    log in page 4 arye

--- Hackacton.py
+++ Hackacton.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    if False:  # TODO logged in<=>has session
+        return render_template('main.html')
+    else:
+        return render_template('login_page.html')
+
+
+@app.route('/hello')
+def hello():
     return render_template('main.html')
 
 
